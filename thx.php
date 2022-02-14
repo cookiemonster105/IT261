b51e1026513260e9414198781b7d034296b20aa0
Wayne Journee <journee@gmail.com>
2022-02-14 12:43:31 -0800

    week6

---
+++ thx.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>🍷Thanks PHP Wine Form🍷</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+  </head>
+  <body>
+    <?php echo '<h2>THANKS!</h2>'; ?> 
+
+    <img src="wine.jpeg"
+     alt="wine glasses clinking">
+  </body>
+</html>
